4246812dc32da43884e2d43622b3702c8b7d6f0a
T ArunTheja <arun9theja@gmail.com>
2022-06-13 03:34:53 +0000

    file_3.sh

---
+++ Linux Advanced/Command Line Arguments/1/file_3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "First arg: $1"
+echo "Second arg: $2"
+
+: '
+The command to execute a file with arguments.
+$ ./file_3.sh hello world
+
+The output will be.
+First arg: hello
+Second arg: world
