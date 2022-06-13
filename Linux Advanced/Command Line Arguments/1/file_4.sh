edb1ce83b2299176e89bd84893e192addacd0feb
T ArunTheja <arun9theja@gmail.com>
2022-06-13 08:20:18 +0000

    file_4.sh

---
+++ Linux Advanced/Command Line Arguments/1/file_4.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+: '
+The command to execute a file with arguments.
+$ ./file_4.sh Zara Ali
+
+The output will be.
+File Name : test.sh
+First Parameter : Zara
+Second Parameter : Ali
+Quoted Values: Zara Ali
+Quoted Values: Zara Ali
+Total Number of Parameters : 2
+'
