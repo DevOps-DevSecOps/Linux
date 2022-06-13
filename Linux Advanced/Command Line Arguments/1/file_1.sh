303d2a3083c17deba0a4c597ed9be87900f98277
T ArunTheja <arun9theja@gmail.com>
2022-06-13 02:57:44 +0000

    file_1.sh

---
+++ Linux Advanced/Command Line Arguments/1/file_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Username: $1";
+echo "Age: $2";
+echo "Full Name: $3";
+
+: '
+The command to execute a file with arguments.
+$ sh file_1.sh  john 25 'John Smith'
+
+The output will be.
+Username : john
+Age: 25
+Full Name: John Smith
+'
