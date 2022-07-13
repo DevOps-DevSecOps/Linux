8f257d37caa9e621a2a2ddb26549cd5c938bafa7
Arun Theja <naguads111@gmail.com>
2022-07-13 17:06:50 +0000

    file_5.sh

---
+++ Linux Advanced/Command Line Arguments/1/file_5.sh
@@ -0,0 +1,11 @@
+echo "Hello World"
+
+echo "The number of parameters $#"                      # '$#' will count total no of arguments passed to script
+
+: '
+The command to execute a file with arguments.
+$ sh file_5.sh 3 4 6 7 9
+
+The output will be.
+Hello World
+The number of parameters 5
